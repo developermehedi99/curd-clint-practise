e67cf769fd182a725b8dae4f83a5b8459a1f5166
Mehedi Hasan <mdmehedihasan20010@gmail.com>
2024-10-24 02:54:28 +0600

    clint side

---
+++ src/components/Users.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Users = () => {
+    const loadedUsers = useLoaderData();
+    const [users, setUsers] = useState(loadedUsers)
+    const handleDelete=_id=>{
+        console.log(_id)
+        fetch(`http://localhost:5000/users/${_id}`,{
+            method:'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            if(data.deletedCount > 0){
+                alert('delete is successfully')
+                const remening = users.filter(user => user._id !== _id);
+                setUsers(remening);
+            }
+        })
+    }
+    return (
+        <div>
+            <h2>usesrs: {users.length}</h2>
+            <div>
+                {
+                    users.map(user => <p key={user._id}>{user.name} : {user._id} <button onClick={()=> handleDelete(user._id)}>X</button> </p>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Users;
